e0cf8f5f091f3c2c8985c18d23c74e38762ee7f1
Haim Cohen <haim1979@gmail.com>
2018-10-21 12:30:26 +0300

    Update check4root.sh
    add color text if you connect as root or not.

--- check4root.sh
+++ check4root.sh
@@ -2,10 +2,12 @@
 # This Script check if you are logged as root or not
 # Haim Cohen 1.1
 
+printf "\033c"
 
-echo "Hello World!"
 if [[ $UID<1 ]]; then
-	echo "okay $USER...You are logged as root. :-)"
+	tput setaf 2; echo "okay $USER...You are logged as root. :-)"
 else
-	echo "opsssss $USER....You are NOT logged as root. :-("
+	tput setaf 1; echo "opsssss $USER....You are NOT logged as root. :-("
 fi
+tput setaf 9
+exit 0
